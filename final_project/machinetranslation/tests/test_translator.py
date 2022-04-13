ca23927c801524b743135c1093d46bd6cf0437fd
Darius Alix-Williams <dalixwill@Burnham.local>
2022-04-13 12:58:39 -0400

    Enable unittests

--- final_project/machinetranslation/tests/test_translator.py
+++ final_project/machinetranslation/tests/test_translator.py
@@ -3,18 +3,29 @@ import sys
 
 sys.path.insert(0, "../translator")
 
-from translator import englishToFrench, frenchToEnglish
+from translator.translator import englishToFrench, frenchToEnglish
 
 class TestEnglishToFrench(unittest.TestCase):
-    def test1(self):
+    def test_assertEqual(self):
         self.assertEqual(englishToFrench('Hello'), 'Bonjour')
         self.assertEqual(englishToFrench(), None)
         self.assertEqual(englishToFrench(""), None)
 
+    def test_assertNotEqual(self):
+        self.assertNotEqual(englishToFrench("Hello"), "Au Revoir")
+        self.assertNotEqual(englishToFrench(""), "")
+        self.assertNotEqual(englishToFrench(None), "Bonjour")
+
 class TestFrenchToEnglish(unittest.TestCase):
-    def test1(self):
+    def test_assertEqual(self):
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello')
         self.assertEqual(frenchToEnglish(), None)
         self.assertEqual(frenchToEnglish(""), None)
 
-unittest.main()
+    def test_assertNotEqual(self):
+        self.assertNotEqual(frenchToEnglish("Au Revoir"), "Hello")
+        self.assertNotEqual(frenchToEnglish(), "")
+        self.assertNotEqual(frenchToEnglish(""), "Hello")
+
+if __name__ == "__main__":
+    unittest.main()
--- final_project/server.py
+++ final_project/server.py
@@ -7,20 +7,17 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
     french_text = translator.translator.englishToFrench(textToTranslate)
     return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
     english_text = translator.translator.frenchToEnglish(textToTranslate)
     return english_text
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
     return render_template("index.html")
 
 if __name__ == "__main__":
